424e975848fc8cf4b2c3fef531f9ace56dfaab5b
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2023-01-25 16:45:39 +0100

    log2 without relative error

---
+++ src/log2.py
@@ -0,0 +1,10 @@
+def calc_log(p):
+    res = 0
+    for n in range(1, 1000000):
+        if n % 2 == 0:
+            res -= 1/n
+        else:
+            res += 1/n
+    return rp(res,p)
+
+print(calc_log(4))
