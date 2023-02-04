e19054dc723f9dccbda96b86bf515c34991cb9cb
Guilhem ARDOUIN <gardouin@enseirb.fr>
2023-02-04 10:43:53 +0100

    relative error log2 done

--- src/log2.py
+++ src/log2.py
@@ -1,4 +1,6 @@
 from decimal_ import rp
+import numpy as np
+import matplotlib.pyplot as plt
 import math
 
 def calc_log(p):
@@ -10,7 +12,21 @@ def calc_log(p):
             res += 1/n
     return rp(res,p)
 
-#print(calc_log(20))
+def delta_log(x,p):
+    return abs(((math.log(x))-calc_log(p)))/abs(math.log(x))
+
+def plot_delta_log():
+    p = np.linspace(1,20,20) # p varie de 1 à 20
+    log_approx = []
+    for i in range(1,21):
+        log_approx.append(delta_log(2,i))
+    plt.plot(p,log_approx)
+    plt.xlabel('valeur de p')
+    plt.ylabel('delta_log(2,p) : erreur relative de log(2)')
+    plt.show()
+    return None
+
+#plot_delta_log()
 
 # NB : représentation erronée à partir de p >= 5
 def test_log():
@@ -48,4 +64,4 @@ def test_log():
 
     return None
 
-test_log()
+#test_log()
