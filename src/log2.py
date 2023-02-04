aadb99902d637a1317a731777ded6da74f3af512
Guilhem ARDOUIN <gardouin@enseirb.fr>
2023-02-04 10:25:41 +0100

    log2 tests: done

--- src/log2.py
+++ src/log2.py
@@ -1,4 +1,5 @@
-from decimal_ import *
+from decimal_ import rp
+import math
 
 def calc_log(p):
     res = 0
@@ -9,4 +10,42 @@ def calc_log(p):
             res += 1/n
     return rp(res,p)
 
-print(calc_log(20))
+#print(calc_log(20))
+
+# NB : représentation erronée à partir de p >= 5
+def test_log():
+
+    x = 2
+    log_python = math.log(x)    
+
+    # p = 1
+    p = 1
+    log_python_rp = rp(log_python,p)
+    log_approx = calc_log(p)
+    assert(log_python_rp == log_approx)
+    print("test_log (p = 1) : \tPASSED")
+
+    # p = 2
+    p = 2
+    log_python_rp = rp(log_python,p)
+    log_approx = calc_log(p)
+    assert(log_python_rp == log_approx)
+    print("test_log (p = 2) : \tPASSED")
+
+    # p = 3
+    p = 3
+    log_python_rp = rp(log_python,p)
+    log_approx = calc_log(p)
+    assert(log_python_rp == log_approx)
+    print("test_log (p = 3) : \tPASSED")
+    
+    # p = 4
+    p = 4
+    log_python_rp = rp(log_python,p)
+    log_approx = calc_log(p)
+    assert(log_python_rp == log_approx)
+    print("test_log (p = 4) : \tPASSED")
+
+    return None
+
+test_log()
