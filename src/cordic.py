d68ee7d7ae5870ef61339ce184fce1629adc2bf4
Guilhem ARDOUIN <gardouin@enseirb.fr>
2023-02-06 13:49:12 +0100

    tests exp done

--- src/cordic.py
+++ src/cordic.py
@@ -32,7 +32,10 @@ def exp(x):
 
 # print(exp(15))
 # print(math.exp(15))
-
+# print(exp(0))
+# print(math.exp(0))
+# print(exp(49))
+# print(math.exp(49))
 
 # NB : valeur fausse pour p >= 13
 def test_ln():
@@ -46,25 +49,25 @@ def test_ln():
     ln_python_rp = rp(ln_python, p)
     ln_approx = rp(ln(x), p)
     assert(ln_python_rp == ln_approx)
-    print("test_arctan (x = 0.5, p = 10) : \t PASSED")
+    print("test_ln (x = 0.5, p = 10) : \t PASSED")
 
     #p = 11
     p = 11
     ln_python_rp = rp(ln_python, p)
     ln_approx = rp(ln(x), p)
     assert(ln_python_rp == ln_approx)
-    print("test_arctan (x = 0.5, p = 11) : \t PASSED")
+    print("test_ln (x = 0.5, p = 11) : \t PASSED")
 
     #p = 12
     p = 12
     ln_python_rp = rp(ln_python, p)
     ln_approx = rp(ln(x), p)
     assert(ln_python_rp == ln_approx)
-    print("test_arctan (x = 0.5, p = 12) : \t PASSED")
+    print("test_ln (x = 0.5, p = 12) : \t PASSED")
 
 
     #x = 5
-    x = 5
+    x = 13
     ln_python = math.log(x)
 
     #p = 10
@@ -72,20 +75,101 @@ def test_ln():
     ln_python_rp = rp(ln_python, p)
     ln_approx = rp(ln(x), p)
     assert(ln_python_rp == ln_approx)
-    print("test_arctan (x = 5, p = 10) : \t PASSED")
+    print("test_ln (x = 5, p = 10) : \t PASSED")
 
     #p = 11
     p = 11
     ln_python_rp = rp(ln_python, p)
     ln_approx = rp(ln(x), p)
     assert(ln_python_rp == ln_approx)
-    print("test_arctan (x = 5, p = 11) : \t PASSED")
+    print("test_ln (x = 5, p = 11) : \t PASSED")
 
     #p = 12
     p = 12
     ln_python_rp = rp(ln_python, p)
     ln_approx = rp(ln(x), p)
     assert(ln_python_rp == ln_approx)
-    print("test_arctan (x = 5, p = 12) : \t PASSED")
+    print("test_ln (x = 5, p = 12) : \t PASSED")
 
-test_ln()
+#test_ln()
+
+def test_exp():
+
+    #x = 0.5
+    x = 0.5
+    exp_python = math.exp(x)
+
+    #p = 10
+    p = 10
+    exp_python_rp = rp(exp_python, p)
+    exp_approx = rp(exp(x), p)
+    assert(exp_python_rp == exp_approx)
+    print("test_exp (x = 0.5, p = 10) : \t PASSED")
+
+    #p = 11
+    p = 11
+    exp_python_rp = rp(exp_python, p)
+    exp_approx = rp(exp(x), p)
+    assert(exp_python_rp == exp_approx)
+    print("test_exp (x = 0.5, p = 11) : \t PASSED")
+
+    #p = 12
+    p = 12
+    exp_python_rp = rp(exp_python, p)
+    exp_approx = rp(exp(x), p)
+    assert(exp_python_rp == exp_approx)
+    print("test_exp (x = 0.5, p = 12) : \t PASSED")
+
+
+    #x = 5
+    x = 5
+    exp_python = math.exp(x)
+
+    #p = 10
+    p = 10
+    exp_python_rp = rp(exp_python, p)
+    exp_approx = rp(exp(x), p)
+    assert(exp_python_rp == exp_approx)
+    print("test_exp (x = 5, p = 10) : \t PASSED")
+
+    #p = 11
+    p = 11
+    exp_python_rp = rp(exp_python, p)
+    exp_approx = rp(exp(x), p)
+    assert(exp_python_rp == exp_approx)
+    print("test_exp (x = 5, p = 11) : \t PASSED")
+
+    #p = 12
+    p = 12
+    exp_python_rp = rp(exp_python, p)
+    exp_approx = rp(exp(x), p)
+    assert(exp_python_rp == exp_approx)
+    print("test_exp (x = 5, p = 12) : \t PASSED")
+
+    #x = -3.03
+    x = -3.03
+    exp_python = math.exp(x)
+
+    #p = 10
+    p = 10
+    exp_python_rp = rp(exp_python, p)
+    exp_approx = rp(exp(x), p)
+    print(exp_approx,exp_python_rp)
+    assert(exp_python_rp == exp_approx)
+    print("test_exp (x = -3.5, p = 10) : \t PASSED")
+
+    #p = 11
+    p = 11
+    exp_python_rp = rp(exp_python, p)
+    exp_approx = rp(exp(x), p)
+    assert(exp_python_rp == exp_approx)
+    print("test_exp (x = -3.5, p = 11) : \t PASSED")
+
+    #p = 12
+    p = 12
+    exp_python_rp = rp(exp_python, p)
+    exp_approx = rp(exp(x), p)
+    assert(exp_python_rp == exp_approx)
+    print("test_exp (x = -3.5, p = 12) : \t PASSED")
+
+#test_exp()
