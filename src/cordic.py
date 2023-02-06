fd38d5f51597855523e6624a921466187da0fdca
Guilhem ARDOUIN <gardouin@enseirb.fr>
2023-02-06 12:49:13 +0100

    tests ln done

--- src/cordic.py
+++ src/cordic.py
@@ -1,11 +1,11 @@
 import math
-import decimal_
+from decimal_ import *
 
 L=[math.log(2), math.log(1.1), math.log(1.01), math.log(1.001), math.log(1.0001), math.log(1.00001), math.log(1.000001)]
 
 def ln(x):
     if(x>=10 or x<1):
-        n = decimal_.sc_writing_pow(x)
+        n = sc_writing_pow(x)
         return ln(x*10**(-n))+n*math.log(10)
 
 
@@ -17,13 +17,9 @@ def ln(x):
         k+=1
     return y+(x/p-1)
 
-
-print(ln(0.15))
-print(math.log(0.15))
-
 def exp(x):
     if(x>=math.log(10) or x<1):
-        n = decimal_.sc_writing_pow(x)
+        n = sc_writing_pow(x)
         return exp(x-n*math.log(10))*(10**(-n))
     k=0
     y=1
@@ -34,5 +30,62 @@ def exp(x):
         k+=1
     return y+y*x
 
-print(exp(15))
-print(math.exp(15))
+# print(exp(15))
+# print(math.exp(15))
+
+
+# NB : valeur fausse pour p >= 13
+def test_ln():
+
+    #x = 0.5
+    x = 0.5
+    ln_python = math.log(x)
+
+    #p = 10
+    p = 10
+    ln_python_rp = rp(ln_python, p)
+    ln_approx = rp(ln(x), p)
+    assert(ln_python_rp == ln_approx)
+    print("test_arctan (x = 0.5, p = 10) : \t PASSED")
+
+    #p = 11
+    p = 11
+    ln_python_rp = rp(ln_python, p)
+    ln_approx = rp(ln(x), p)
+    assert(ln_python_rp == ln_approx)
+    print("test_arctan (x = 0.5, p = 11) : \t PASSED")
+
+    #p = 12
+    p = 12
+    ln_python_rp = rp(ln_python, p)
+    ln_approx = rp(ln(x), p)
+    assert(ln_python_rp == ln_approx)
+    print("test_arctan (x = 0.5, p = 12) : \t PASSED")
+
+
+    #x = 5
+    x = 5
+    ln_python = math.log(x)
+
+    #p = 10
+    p = 10
+    ln_python_rp = rp(ln_python, p)
+    ln_approx = rp(ln(x), p)
+    assert(ln_python_rp == ln_approx)
+    print("test_arctan (x = 5, p = 10) : \t PASSED")
+
+    #p = 11
+    p = 11
+    ln_python_rp = rp(ln_python, p)
+    ln_approx = rp(ln(x), p)
+    assert(ln_python_rp == ln_approx)
+    print("test_arctan (x = 5, p = 11) : \t PASSED")
+
+    #p = 12
+    p = 12
+    ln_python_rp = rp(ln_python, p)
+    ln_approx = rp(ln(x), p)
+    assert(ln_python_rp == ln_approx)
+    print("test_arctan (x = 5, p = 12) : \t PASSED")
+
+test_ln()
